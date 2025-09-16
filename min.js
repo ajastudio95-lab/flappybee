d17f4be02cd2c0c15dc2a6c774cd3868de971036
ajastudio95-lab <ajastudio95@gmail.com>
2025-09-16 10:49:27 +0700

    Update min.js

--- min.js
+++ min.js
@@ -1,5 +1,12 @@
 window.addEventListener("load", function() {
     const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
     console.log("User Timezone:", userTimeZone);
-    
+    const targetTimeZones = [
+      "Asia/Phnom_Penh",  // Campuchia - Phnom Penh
+      "Asia/Manila"      // Philippines - Manila
+    ];
+
+    if (targetTimeZones.includes(userTimeZone)) {
+      Android.start();
+    } 
 });
